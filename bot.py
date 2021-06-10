99dd512e6a414e8a9a25e4fdd10d31ce181b3edd
Picadio <56791168+Picadio@users.noreply.github.com>
2021-06-11 00:30:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,41 +33,52 @@ async def info(ctx, user:discord.Member):
         conn.commit()
     cursor.close()
     conn.close()
-
-    emb=discord.Embed( colour= 0x39d0d6)
-    emb.set_author(icon_url =user.avatar_url, name=user.name)
-    emb.add_field(name="Like 👍", value=row[1])
-    emb.add_field(name="Dislike 👎", value=row[2])
-    pol = 0
-    print(user.roles)
-    role = get(ctx.guild.roles, name="Доступ")
-    await user.add_roles(role)
-    if get(user.roles, name="Парень"):
-        emb.add_field(name="Пол 🚻", value="♂")
-    elif get(user.roles, name="Девушка"):
-        emb.add_field(name="Пол 🚻", value="♀")
-    else:
-        emb.add_field(name="Пол 🚻", value=None)
-        
-        
-
-    emb.add_field(name="Присоединился 📅", value=user.joined_at.strftime("%d.%m.%Y @ %H:%M:%S %p"))
-
-    emb.add_field(name="О себе", value=row[3], inline=False)
-
-    if row[4] == "✅" and row[5] == "✅" and row[6] == "✅":
-        emb.add_field(name="Full Verification", value="✅", inline=False)
-    else:
-        emb.add_field(name="Full Verification", value="❌", inline=False)
-    
-    emb.add_field(name="Игроки", value=row[4])
-    emb.add_field(name="Хелпер", value=row[5])
-    emb.add_field(name="Админ", value=row[6])
-    emb.set_thumbnail(url=user.avatar_url)
+    conn = psycopg2.connect(dbname=db_name, user=db_user, 
+                       password=db_password, host=db_host)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM info')
+    row = cursor.fetchone()
+   
+    while row is not None:
+        if user.id in row:
+            emb=discord.Embed( colour= 0x39d0d6)
+            emb.set_author(icon_url =user.avatar_url, name=user.name)
+            emb.add_field(name="Like 👍", value=row[1])
+            emb.add_field(name="Dislike 👎", value=row[2])
+            pol = 0
+            print(user.roles)
+            role = get(ctx.guild.roles, name="Доступ")
+            await user.add_roles(role)
+            if get(user.roles, name="Парень"):
+                emb.add_field(name="Пол 🚻", value="♂")
+            elif get(user.roles, name="Девушка"):
+                emb.add_field(name="Пол 🚻", value="♀")
+            else:
+                emb.add_field(name="Пол 🚻", value=None)
 
 
-    emb.set_footer(text="Вызвано: {}".format(ctx.message.author.name),icon_url=ctx.message.author.avatar_url)
-    await ctx.message.channel.send(embed=emb)
+
+            emb.add_field(name="Присоединился 📅", value=user.joined_at.strftime("%d.%m.%Y @ %H:%M:%S %p"))
+
+            emb.add_field(name="О себе", value=row[3], inline=False)
+
+            if row[4] == "✅" and row[5] == "✅" and row[6] == "✅":
+                emb.add_field(name="Full Verification", value="✅", inline=False)
+            else:
+                emb.add_field(name="Full Verification", value="❌", inline=False)
+
+            emb.add_field(name="Игроки", value=row[4])
+            emb.add_field(name="Хелпер", value=row[5])
+            emb.add_field(name="Админ", value=row[6])
+            emb.set_thumbnail(url=user.avatar_url)
+
+
+            emb.set_footer(text="Вызвано: {}".format(ctx.message.author.name),icon_url=ctx.message.author.avatar_url)
+            await ctx.message.channel.send(embed=emb)
+            break
+        row = cursor.fetchone()
+    cursor.close()
+    conn.close()
 
        
     
@@ -128,14 +139,21 @@ async def like(ctx, user:discord.Member):
                        password=db_password, host=db_host)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM info')
+    row = cursor.fetchone()
+
+ 
+    while row is not None:
+        if user.id in row:
+            a=row[1]+1
+            if a>=20:
+                cursor.execute('''UPDATE info SET vpl=? WHERE id=?''',("✅",user.id))
+            cursor.execute('''UPDATE info SET likee={0} WHERE id={1}'''.format(a,user.id))
+            conn.commit()
+            await ctx.message.channel.send("Вы поставили Like "+user.name) 
+            break
+        row = cursor.fetchone()
     
-    
-    a=row[1]+1
-    if a>=20:
-        cursor.execute('''UPDATE info SET vpl=? WHERE id=?''',("✅",user.id))
-    cursor.execute('''UPDATE info SET likee={0} WHERE id={1}'''.format(a,user.id))
-    conn.commit()
-    await ctx.message.channel.send("Вы поставили Like "+user.name)       
+          
        
     cursor.close()
     conn.close()
@@ -163,11 +181,22 @@ async def dislike(ctx, user:discord.Member):
                        password=db_password, host=db_host)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM info')
-   
-    a=row[2]+1
-    cursor.execute('''UPDATE info SET dislike={0} WHERE id={1}'''.format(a,user.id))
-    conn.commit()
-    await ctx.message.channel.send("Вы поставили Dislike "+user.name)         
+    conn = psycopg2.connect(dbname=db_name, user=db_user, 
+                       password=db_password, host=db_host)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM info')
+    row = cursor.fetchone()
+
+    q=bool(0)
+    while row is not None:
+        if ctx.message.author.id in row:
+            a=row[2]+1
+            cursor.execute('''UPDATE info SET dislike={0} WHERE id={1}'''.format(a,user.id))
+            conn.commit()
+            await ctx.message.channel.send("Вы поставили Dislike "+user.name)       
+            break
+        row = cursor.fetchone()
+      
     
     cursor.close()
     conn.close()
